82c24d2835f226e835e7d6fb5ed064bd3901f9f6
abarrios <agusab2000@gmail.com>
2025-01-16 21:23:23 +0100

    change environment URL

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.models';
 import { TrackService } from '@modules/tracks/services/track.service';
 import { Subscription } from 'rxjs';
@@ -6,39 +6,40 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-tracks-page',
   templateUrl: './tracks-page.component.html',
-  styleUrls: ['./tracks-page.component.css']
+  styleUrls: ['./tracks-page.component.css'],
 })
-export class TracksPageComponent implements OnInit,OnDestroy {
-tracksTrending:Array<TrackModel> = []
-tracksRandom:Array<TrackModel> = []
+export class TracksPageComponent implements OnInit, OnDestroy {
+  tracksTrending: Array<TrackModel> = [];
+  tracksRandom: Array<TrackModel> = [];
 
-observerList$:Array<Subscription> =[];
+  observerList$: Array<Subscription> = [];
 
-titleSmall:string ='La mejor musica del mundo'
-titleBig:string ='Lo mejor de Electronica'
+  titleSmall: string = 'La mejor musica del mundo';
+  titleBig: string = 'Lo mejor de Electronica';
 
-  constructor(private trackService: TrackService){}
+  constructor(private trackService: TrackService) {}
 
   ngOnInit(): void {
     this.loadDataAll();
     this.loadDataRandom();
   }
 
-  loadDataAll():void{
-    this.trackService.getAllTracks$().subscribe((res:TrackModel[]) => {
-      this.tracksTrending =res;
-    })
+  loadDataAll(): void {
+    this.trackService.getAllTracks$().subscribe((res: TrackModel[]) => {
+      this.tracksTrending = res;
+    });
   }
 
-  loadDataRandom():void{
-    this.trackService.getAllRandom$().subscribe((res:TrackModel[]) =>{
-      this.tracksRandom = res;
-    },err => {
-      console.log('error de conexion');
-    })
+  loadDataRandom(): void {
+    this.trackService.getAllRandom$().subscribe(
+      (res: TrackModel[]) => {
+        this.tracksRandom = res;
+      },
+      (err) => {
+        console.log('error de conexion');
+      }
+    );
   }
 
-  ngOnDestroy(): void {
-  }
-
+  ngOnDestroy(): void {}
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    api: 'https://api-spotify-leifer.herokuapp.com/api/1.0'
-  };
+  production: true,
+  api: 'https://api-spotify-clone.vercel.app/api/1.0',
+};
